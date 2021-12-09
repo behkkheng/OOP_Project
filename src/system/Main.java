f29c0381c7275d0c603f19f818943c69fae7fafb
user <user@LAPTOP-VOJM5CHA>
2021-12-09 23:42:04 +0800

    hi

--- src/system/Main.java
+++ src/system/Main.java
@@ -1,7 +1,7 @@
 package system;
 
 public class Main {
-
+//hi
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Test pull from behkkheng laptop");
