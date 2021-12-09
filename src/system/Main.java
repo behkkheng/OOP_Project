b5440dfc75550acb13ca74c06570c05747184acf
behkkheng <behkkheng@gmail.com>
2021-12-09 21:59:23 +0800

    behkkheng laptop

--- src/system/Main.java
+++ src/system/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Pull from behkkheng laptop");
+        System.out.println("test pull from behkkheng laptop");
     }
 }
