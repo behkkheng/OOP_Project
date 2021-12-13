41bfc3f027ac6fae39cba444517feb38f5b2d68e
behkkheng <behkkheng@gmail.com>
2021-12-13 15:52:50 +0800

    Main page remain top navigation bar

--- src/system/WelcomePage.java
+++ src/system/WelcomePage.java
@@ -6,14 +6,17 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import javax.swing.JLabel;
+import javax.swing.JMenuBar;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import java.awt.Font;
 import javax.swing.SwingConstants;
 import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-public class WelcomePage {
+public class WelcomePage extends JFrame{
 
 	private JFrame frame;
 
@@ -54,15 +57,24 @@ public class WelcomePage {
 		JLabel pressToContinue = new JLabel("Press anywhere to continue...");
 		pressToContinue.setHorizontalAlignment(SwingConstants.CENTER);
 		pressToContinue.setFont(new Font("Segoe UI Semibold", Font.BOLD, 36));
-		pressToContinue.setBounds(416, 329, 480, 50);
+		pressToContinue.setBounds(378, 329, 480, 50);
 		frame.getContentPane().add(pressToContinue);
 		
 		JLabel backgroundPicture = new JLabel("");
+		backgroundPicture.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				MainPages.main(null);
+				frame.dispose();
+			}
+		});
+
 		backgroundPicture.setForeground(Color.BLACK);
 		backgroundPicture.setHorizontalAlignment(SwingConstants.CENTER);
 		backgroundPicture.setIcon(new ImageIcon(getClass().getResource("./food.jpg")));
 		backgroundPicture.setBounds(0, 0, 1266, 683);
 		frame.getContentPane().add(backgroundPicture);
 		
+
 	}
 }
